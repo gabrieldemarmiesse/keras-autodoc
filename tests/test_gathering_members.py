94ec945082ce18ac2c208277fbd846337385ae75
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-10-27 12:07:16 +0000

    Returning strings work, but it's missing some tests.

--- tests/test_gathering_members.py
+++ tests/test_gathering_members.py
@@ -5,12 +5,25 @@ from .dummy_package import dummy_module
 
 
 def test_get_module_functions():
-    assert set(get_functions(dummy_module)) == {dummy_module.to_categorical}
+    expected = {dummy_module.to_categorical}
+    computed = set(get_functions(dummy_module, return_strings=False))
+    assert expected == computed
+
+
+def test_get_module_functions_to_str():
+    expected = {'tests.dummy_package.dummy_module.to_categorical'}
+    assert set(get_functions(dummy_module)) == expected
+
+
+def test_get_module_functions_from_str_to_str():
+    expected = {'tests.dummy_package.to_categorical'}
+    computed = set(get_functions('tests.dummy_package'))
+    assert computed == expected
 
 
 def test_get_module_classes():
     expected = {dummy_module.ImageDataGenerator, dummy_module.Dense}
-    assert set(get_classes(dummy_module)) == expected
+    assert set(get_classes(dummy_module, return_strings=False)) == expected
 
 
 def test_get_class_methods():
@@ -18,4 +31,6 @@ def test_get_class_methods():
         dummy_module.ImageDataGenerator.flow,
         dummy_module.ImageDataGenerator.flow_from_directory
     }
-    assert set(get_methods(dummy_module.ImageDataGenerator)) == expected
+    computed = get_methods(dummy_module.ImageDataGenerator, return_strings=False)
+    computed = set(computed)
+    assert computed == expected
